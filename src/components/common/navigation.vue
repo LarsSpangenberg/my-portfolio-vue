3d96d955365a295fe62055e4487c41b74177aa3b
Lars Spangenberg <lazerus88@gmail.com>
2020-06-20 02:29:36 -0400

    removed unneeded files created by vue cli

--- src/components/common/navigation.vue
+++ src/components/common/navigation.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" 
-              data-target="#navbarTogglerDemo03" 
-              aria-controls="navbarTogglerDemo03" 
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+              data-target="#navbarTogglerDemo03"
+              aria-controls="navbarTogglerDemo03"
               aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
